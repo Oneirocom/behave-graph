4c51c69294b70083bafe36145456948468fcfdf6
Michael <m.sharpe@project89.org>
2023-12-06 16:04:03 -0800

    Convert delay node to new format

--- packages/core/src/Profiles/Core/Time/Delay.ts
+++ packages/core/src/Profiles/Core/Time/Delay.ts
@@ -1,65 +1,111 @@
-import { Engine } from '../../../Execution/Engine.js';
-import { IGraph } from '../../../Graphs/Graph.js';
-import { AsyncNode } from '../../../Nodes/AsyncNode.js';
 import {
-  NodeDescription,
-  NodeDescription2
-} from '../../../Nodes/Registry/NodeDescription.js';
-import { Socket } from '../../../Sockets/Socket.js';
+  NodeCategory,
+  makeAsyncNodeDefinition
+} from '../../../Nodes/NodeDefinitions.js';
 
-// ASYNC - asynchronous evaluation
-// also called "delay"
-
-export class Delay extends AsyncNode {
-  public static Description = new NodeDescription2({
-    typeName: 'time/delay',
-    otherTypeNames: ['flow/delay'],
-    category: 'Time',
-    label: 'Delay',
-    factory: (description, graph, config, id) =>
-      new Delay(description, graph, config, id)
-  });
-
-  constructor(
-    description: NodeDescription,
-    graph: IGraph,
-    config: Record<string, unknown>,
-    id: string
-  ) {
-    super(
-      description,
-      graph,
-      [new Socket('flow', 'flow'), new Socket('float', 'duration', 1)],
-      [new Socket('flow', 'flow')],
-      config,
-      id
-    );
-  }
-
-  private timeoutPending = false;
-
-  triggered(
-    engine: Engine,
-    triggeringSocketName: string,
-    finished: () => void
-  ) {
+// New format
+export const Delay = makeAsyncNodeDefinition({
+  typeName: 'time/delay',
+  otherTypeNames: ['flow/delay'],
+  category: NodeCategory.Time,
+  label: 'Delay',
+  in: {
+    flow: {
+      valueType: 'flow'
+    },
+    duration: {
+      valueType: 'float',
+      defaultValue: 1
+    }
+  },
+  out: {
+    flow: 'flow'
+  },
+  initialState: {
+    timeoutPending: false
+  },
+  triggered: ({ state, finished = () => {}, commit, read }) => {
     // if there is a valid timeout running, leave it.
-    if (this.timeoutPending) {
-      return;
+    if (state.timeoutPending) {
+      return {
+        timeoutPending: true
+      };
     }
 
     // otherwise start it.
-    this.timeoutPending = true;
+    state.timeoutPending = true;
+
     setTimeout(() => {
       // check if cancelled
-      if (!this.timeoutPending) return;
-      this.timeoutPending = false;
-      engine.commitToNewFiber(this, 'flow');
+      if (!state.timeoutPending) return;
+      state.timeoutPending = false;
+      commit('flow');
       finished();
-    }, this.readInput<number>('duration') * 1000);
-  }
+    }, read<number>('duration') * 1000);
 
-  dispose() {
-    this.timeoutPending = false;
+    return {
+      timeoutPending: true
+    };
+  },
+  dispose: () => {
+    return {
+      timeoutPending: false
+    };
   }
-}
+});
+
+// Old format
+// export class Delay extends AsyncNode {
+//   public static Description = new NodeDescription2({
+//     typeName: 'time/delay',
+//     otherTypeNames: ['flow/delay'],
+//     category: 'Time',
+//     label: 'Delay',
+//     factory: (description, graph, config, id) =>
+//       new Delay(description, graph, config, id)
+//   });
+
+//   constructor(
+//     description: NodeDescription,
+//     graph: IGraph,
+//     config: Record<string, unknown>,
+//     id: string
+//   ) {
+//     super(
+//       description,
+//       graph,
+//       [new Socket('flow', 'flow'), new Socket('float', 'duration', 1)],
+//       [new Socket('flow', 'flow')],
+//       config,
+//       id
+//     );
+//   }
+
+//   private timeoutPending = false;
+
+//   triggered(
+//     engine: Engine,
+//     triggeringSocketName: string,
+//     finished: () => void
+//   ) {
+//     // if there is a valid timeout running, leave it.
+//     if (this.timeoutPending) {
+//       return;
+//     }
+
+//     // otherwise start it.
+//     this.timeoutPending = true;
+//     setTimeout(() => {
+//       // check if cancelled
+//       if (!this.timeoutPending) return;
+//       this.timeoutPending = false;
+//       console.log('delay finished!!!!!!!!!');
+//       engine.commitToNewFiber(this, 'flow');
+//       finished();
+//     }, this.readInput<number>('duration') * 1000);
+//   }
+
+//   dispose() {
+//     this.timeoutPending = false;
+//   }
+// }
