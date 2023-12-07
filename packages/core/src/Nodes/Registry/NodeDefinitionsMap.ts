46c220113d3f0fee1928d588fc7058e16c291b94
Michael <m.sharpe@project89.org>
2023-12-06 16:02:04 -0800

    Allow config in NodeDefinition type

--- packages/core/src/Nodes/Registry/NodeDefinitionsMap.ts
+++ packages/core/src/Nodes/Registry/NodeDefinitionsMap.ts
@@ -1,7 +1,7 @@
 import { IHasNodeFactory, INodeDefinition } from '../NodeDefinitions.js';
 
 export type NodeDefinition = IHasNodeFactory &
-  Pick<INodeDefinition, 'typeName' | 'otherTypeNames'>;
+  Pick<INodeDefinition, 'typeName' | 'otherTypeNames' | 'configuration'>;
 
 export type NodeDefinitionsMap = {
   readonly [type: string]: NodeDefinition;
